a2dcb01251d2f66bf89138aff8e08d40c6398510
Boyd Hemphill <boyd@feedmagnet.com>
2013-07-01 12:33:25 -0500

    [COOK-3246] Create subdirs when necessary
    Some services, such as byobu have a directory structure (usually just a
    `~/.byobu` ).  This commit allows for the creation of the directory if it
    does not exist.

--- providers/manage.rb
+++ providers/manage.rb
@@ -143,10 +143,29 @@ action :create do
         # If the data bag contains a custom_env and the custom_env input is true
         # layout files as specified.
         if new_resource.custom_env and u['custom_env']
+
+          # We will likely need to create parent directories for things like
+          # .byobu and .ssh so Pathname is handy
+          require 'pathname'
+
           # if there are files specified in the data bag then place them
           # per the value associated with the filename.
           if u['custom_env']['files']
             u['custom_env']['files'].each do |file,info|
+
+              parent_path = Pathname.new(info['path']).parent.to_s
+
+              # If the directory does not exist, create it in the users home
+              # directory
+              unless parent_path == '.'
+                directory "#{home_dir}/#{parent_path}" do
+                  group u['username']
+                  mode '00755'
+                  owner u['username']
+                  recursive true
+                end
+              end
+
               cookbook_file "#{home_dir}/#{info['path']}" do
                 cookbook new_resource.wrapper_cookbook
                 group u['username']
@@ -156,10 +175,25 @@ action :create do
               end
             end
           end
+
           # if there are templates specified in the data bag then place them
           # per the value associated with the filename.
           if u['custom_env']['templates']
             u['custom_env']['templates'].each do |file,info|
+
+              parent_path = Pathname.new(info['path']).parent.to_s
+
+              # If the directory does not exist, create it in the users home
+              # directory
+              unless parent_path == '.'
+                directory "#{home_dir}/#{parent_path}" do
+                  group u['username']
+                  mode '00755'
+                  owner u['username']
+                  recursive true
+                end
+              end
+
               template "#{home_dir}/#{info['path']}" do
                 cookbook new_resource.wrapper_cookbook
                 group u['username']

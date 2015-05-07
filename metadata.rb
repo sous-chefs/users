2d4675a4a3aa0d6d9303a96ef3f9139d36b1477a
Giorgos Saslis <g.saslis@gmail.com>
2015-05-07 18:28:33 +0300

    Support Chef < 12
    source_url and issues_url are Chef 12 attributes that do not have backwards compatibility, so only set them if running with a version of Chef that supports them.

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license          "Apache 2.0"
 description      "Creates users from a databag search"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.8.3"
-source_url       "https://github.com/opscode-cookbooks/users"
-issues_url       "https://github.com/opscode-cookbooks/users/issues"
+source_url       "https://github.com/opscode-cookbooks/users" if respond_to?(:source_url)
+issues_url       "https://github.com/opscode-cookbooks/users/issues" if respond_to?(:issues_url)
 recipe           "users", "Empty recipe for including LWRPs"
 recipe           "users::sysadmins", "Create and manage sysadmin group"
 

0f5a262f2e5e327b584b07cb8119ad1a5797c2df
Tim Smith <tsmith84@gmail.com>
2015-12-12 12:53:12 -0800

    Bump up GID and UIDs for travis

--- test/fixtures/cookbooks/users_test/recipes/test_home_dir.rb
+++ test/fixtures/cookbooks/users_test/recipes/test_home_dir.rb
@@ -1,11 +1,11 @@
 users_manage 'testgroup' do
-  group_id 1000
+  group_id 3000
   action [:remove, :create]
   data_bag 'test_home_dir'
 end
 
 users_manage 'nfsgroup' do
-  group_id 2000
+  group_id 4000
   action [:remove, :create]
   data_bag 'test_home_dir'
   manage_nfs_home_dirs false

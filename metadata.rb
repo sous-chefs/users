c84ff6727646497fb6f76071ef50184313f875df
Tim Smith <tsmith@chef.io>
2017-02-15 16:44:42 -0800

    Convert manage LWRP to a custom resource and require Chef 12.5+
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -15,4 +15,4 @@ end
 
 source_url   'https://github.com/chef-cookbooks/users'
 issues_url   'https://github.com/chef-cookbooks/users/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.5' if respond_to?(:chef_version)

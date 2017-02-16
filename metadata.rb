9e2aa3cebe3dc1a6b9e5aea9ffe784e41cff3c44
Tim Smith <tsmith@chef.io>
2017-02-15 16:45:34 -0800

    Add suse as a supported platform in the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Creates users from a databag search'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '4.0.3'
 
-%w( ubuntu debian redhat centos fedora freebsd mac_os_x scientific oracle amazon zlinux ).each do |os|
+%w( ubuntu debian redhat centos fedora freebsd mac_os_x scientific oracle amazon zlinux suse opensuse opensuseleap ).each do |os|
   supports os
 end
 

14e73548f0048aea8f7616e4d8e00668c884f761
Tim Smith <tsmith@chef.io>
2017-02-15 16:45:13 -0800

    Remove recipe descriptions in metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,6 @@ description      'Creates users from a databag search'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '4.0.3'
 
-recipe           'users::default', 'Empty recipe'
-recipe           'users::sysadmins', 'Deprecated recipe to create and manage sysadmin group.'
-
 %w( ubuntu debian redhat centos fedora freebsd mac_os_x scientific oracle amazon zlinux ).each do |os|
   supports os
 end

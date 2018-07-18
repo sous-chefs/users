6fa13a657bac23e7bcb4695ef81f657a05fb559f
Tim Smith <tsmith@chef.io>
2018-07-18 09:30:32 -0700

    Disable FC022 which is a Foodcritic bug
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/manage.rb
+++ resources/manage.rb
@@ -102,7 +102,7 @@ action :create do
       # use the keyfile defined in the databag or fallback to the standard file in the home dir
       key_file = u['authorized_keys_file'] || "#{home_dir}/.ssh/authorized_keys"
 
-      template key_file do
+      template key_file do # ~FC022
         source 'authorized_keys.erb'
         cookbook new_resource.cookbook
         owner u['uid'] ? validate_id(u['uid']) : u['username']
